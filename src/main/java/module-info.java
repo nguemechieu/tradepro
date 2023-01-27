79176167ba31c2655b9592d46c468d5f72aa7888
nguem <nguemechieu@live.com>
2023-01-27 09:19:14 -0500

    runcvb

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,15 @@ module org.tradepro.tradepro {
     requires javafx.controls;
     requires javafx.fxml;
     requires org.jetbrains.annotations;
+    requires org.junit.jupiter.api;
+    requires org.slf4j;
 
+    requires org.eclipse.sisu.plexus;
+
+    requires jdk.internal.vm.compiler.management;
+ requires  java.logging;
+ requires java.datatransfer;
+ requires java.sql;
 
     opens org.tradepro.tradepro to javafx.fxml;
     exports org.tradepro.tradepro;
