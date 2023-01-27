a23bba3ece9ab1eba57ba7c82f55f8400bcda873
nguem <nguemechieu@live.com>
2023-01-27 11:48:32 -0500

    run

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,16 @@
 module org.tradepro.tradepro {
+requires java.compiler;
+    requires javafx.web;
+    requires javafx.swing;
+
     requires javafx.controls;
     requires javafx.fxml;
     requires org.jetbrains.annotations;
     requires org.junit.jupiter.api;
     requires org.slf4j;
-
+    requires javafx.svg;
+requires  javafx.graphics;
+requires javafx.media;
     requires jdk.internal.vm.compiler.management;
  requires  java.logging;
  requires java.datatransfer;
--- src/main/java/org/tradepro/tradepro/Main.java
+++ src/main/java/org/tradepro/tradepro/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     @Override
     public void start(@NotNull Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
